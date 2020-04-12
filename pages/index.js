7b37c2cbb786d75888f97afb8c8f93cab1905540
kakastefane <karistonstefane@hotmail.com>
2020-04-12 12:20:16 -0300

    criado sistema de login com auth0

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ const Index = () => {
   return (
 		<div>
 			<h1>Home</h1>
+			<a href='/api/login' className='py-4 px-2 rounded bg-pink-800 font-bold shadow-lg hover:shadow-xl block w-1/4 text-center mx-auto text-white'>Comece por aqui</a>
 		</div>
 	)
 }
